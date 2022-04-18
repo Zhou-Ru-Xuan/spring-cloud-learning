681e90ac357997a3f3f1e29861fd4272344e755c
zhouruxuan <1205295974@qq.com>
2022-04-18 19:20:46 +0800

    补上turbine

--- gateway9527/src/main/java/org/zhouruxuan/gateway9527/GateWayMain9527.java
+++ gateway9527/src/main/java/org/zhouruxuan/gateway9527/GateWayMain9527.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@SpringBootApplication
+@SpringBootApplication()
 @EnableEurekaClient
 public class GateWayMain9527 {
     public static void main(String[] args) {
